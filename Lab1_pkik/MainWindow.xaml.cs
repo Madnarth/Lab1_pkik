c7db9daf2ee62c7f0be6dbcbe8c7a2b32e22295a
Madnarth <corwyn.blade@gmail.com>
2018-05-09 18:06:04 +0200

    v 1.0

--- Lab1_pkik/MainWindow.xaml.cs
+++ Lab1_pkik/MainWindow.xaml.cs
@@ -52,21 +52,146 @@ namespace Lab1_pkik
         {
             tbB.Text = TextIsChanged(tbB.Text);
         }
+        private void tbBn_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tbBn.Text = TextIsChanged(tbBn.Text);
+        }
+        private void tbN_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tbN.Text = TextIsChanged(tbN.Text);
+        }
+        private void tbM_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tbM.Text = TextIsChanged(tbM.Text);
+        }
+        private void tbNn_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tbNn.Text = TextIsChanged(tbNn.Text);
+        }
+        private void tbK_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tbK.Text = TextIsChanged(tbK.Text);
+        }
         #endregion
         List<int> Sito;
+        #region Click Methods
         private void bPrimeNum_Click(object sender, RoutedEventArgs e)
         {
-            if (IsPrime(Int32.Parse(tbPrimeNum.Text)))
+            try
             {
-                labIsPrime.Content = "pierwsza";
-                labIsPrime.Foreground = new SolidColorBrush(Colors.Green);
+                if (IsPrime(Int32.Parse(tbPrimeNum.Text)))
+                {
+                    labIsPrime.Content = "pierwsza";
+                    labIsPrime.Foreground = new SolidColorBrush(Colors.Green);
+                }
+                else
+                {
+                    labIsPrime.Content = "nie pierwsza";
+                    labIsPrime.Foreground = new SolidColorBrush(Colors.Red);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                labIsPrime.Content = "nie pierwsza";
-                labIsPrime.Foreground = new SolidColorBrush(Colors.Red);
+                MessageBox.Show(ex.Message);
             }
         }
+        private void bSieveOfErato_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Sito = new List<int>();
+                tblPrimes.Text = "";
+                int max = int.Parse(tbRange.Text) + 1;
+                bool[] is_prime = MakeSieve(max);
+                for (int i = 2; i < max; i++)
+                    if (is_prime[i]) Sito.Add(i);
+                foreach (var item in Sito)
+                {
+                    tblPrimes.Text += item + " ";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void bSaveToFile_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text file (*.txt)|*.txt|C# file (*.cs)|*.cs";
+                if (saveFileDialog.ShowDialog() == true)
+                    File.WriteAllText(saveFileDialog.FileName, "Liczby pierwsze dla przedziału (" + tbRange.Text + ") obliczone metodą sita Eratostenesa:" + Environment.NewLine + tblPrimes.Text);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void bCalcGCD_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                switch (combMethod.SelectedIndex)
+                {
+                    case 0:
+                        {
+                            labGCD.Content = GCD(Int32.Parse(tbA.Text), Int32.Parse(tbB.Text));
+                            break;
+                        }
+                    case 1:
+                        {
+                            int[] temp = ExtendedGDC(Int32.Parse(tbA.Text), Int32.Parse(tbB.Text));
+                            labGCD.Content = "k=" + temp[0] + " u=" + temp[1] + " v=" + temp[2];
+                            break;
+                        }
+                    default: break;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void bCalcPowModFast_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                labPowModFast.Content = PowerModFast(Int32.Parse(tbBn.Text), Int32.Parse(tbN.Text), Int32.Parse(tbM.Text));
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void bCalcMiller_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (MillerRabin(Int32.Parse(tbNn.Text), Int32.Parse(tbK.Text)))
+                {
+                    labMiller.Content = "prawdopodobnie pierwsza";
+                    labMiller.Foreground = new SolidColorBrush(Colors.Green);
+                }
+                else
+                {
+                    labMiller.Content = "prawdopodobnie nie pierwsza";
+                    labMiller.Foreground = new SolidColorBrush(Colors.Red);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion
         public static bool IsPrime(int _number)
         {
             if (_number == 1) return false;
@@ -101,27 +226,7 @@ namespace Lab1_pkik
             }
             return is_prime;
         }
-        private void bSieveOfErato_Click(object sender, RoutedEventArgs e)
-        {
-            Sito = new List<int>();
-            tblPrimes.Text = "";
-            int max = int.Parse(tbRange.Text) + 1;
-            bool[] is_prime = MakeSieve(max);
-            for (int i = 2; i < max; i++)
-                if (is_prime[i])  Sito.Add(i);
-            foreach (var item in Sito)
-            {
-                tblPrimes.Text += item + " ";
-            }
-        }
-
-        private void bSaveToFile_Click(object sender, RoutedEventArgs e)
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Text file (*.txt)|*.txt|C# file (*.cs)|*.cs";
-            if (saveFileDialog.ShowDialog() == true)
-                File.WriteAllText(saveFileDialog.FileName, "Liczby pierwsze dla przedziału (" + tbRange.Text + ") obliczone metodą sita Eratostenesa:" + Environment.NewLine + tblPrimes.Text);
-        }
+        
         static int GCD(int _a, int _b)
         {
             return _b == 0 ? _a : GCD(_b, _a % _b);
@@ -156,23 +261,79 @@ namespace Lab1_pkik
             value[0] = b; value[1] = yn; value[2] = xn;
             return value;
         }
-        private void bCalcGCD_Click(object sender, RoutedEventArgs e)
+        public static int PowerModFast(int a, int b, int m)
         {
-            switch(combMethod.SelectedIndex)
+            int i;
+            long result = 1;
+            long x = a % m;
+
+            for (i = 1; i <= b; i <<= 1)
             {
-                case 0:
+                x %= m;
+                if ((b & i) != 0)
+                {
+                    result *= x;
+                    result %= m;
+                }
+                x *= x;
+            }
+
+            return (int)result;
+        }
+       
+        private static int[] powerOf2 = {
+            1<<0,  1<<1,  1<<2,  1<<3,  1<<4,  1<<5,  1<<6,
+            1<<7,  1<<8,  1<<9,  1<<10, 1<<11, 1<<12, 1<<13,
+            1<<14, 1<<15, 1<<16, 1<<17, 1<<18, 1<<19, 1<<20,
+            1<<21, 1<<22, 1<<23, 1<<24, 1<<25, 1<<26, 1<<27,
+            1<<28, 1<<29, 1<<30
+        };
+        private static Boolean MillerRabin(int n, int k)
+        {
+            int s = 0;
+            int s2 = 1;
+            int a, d, i, r, prime;
+            Random rand = new Random();
+
+            if (n < 4)
+            {
+                return true;
+            }
+            if (n % 2 == 0)
+            {
+                return false;
+            }
+
+            // calculate s and d
+            while ((s2 & (n - 1)) == 0)
+            {
+                s += 1;
+                s2 <<= 1;
+            }
+            d = n / s2;
+
+            // try k times
+            for (i = 0; i < k; i++)
+            {
+                a = rand.Next(n - 1) + 1;
+                if (PowerModFast(a, d, n) != 1)
+                {
+                    prime = 0;
+                    for (r = 0; r <= s - 1; r++)
                     {
-                        labGCD.Content = GCD(Int32.Parse(tbA.Text), Int32.Parse(tbB.Text));
-                        break;
+                        if (PowerModFast(a, powerOf2[r] * d, n) == n - 1)
+                        {
+                            prime = 1;
+                            break;
+                        }
                     }
-                case 1:
+                    if (prime == 0)
                     {
-                        int[] temp = ExtendedGDC(Int32.Parse(tbA.Text), Int32.Parse(tbB.Text));
-                        labGCD.Content = "k=" + temp[0] + " u=" + temp[1] + " v=" + temp[2];
-                        break;
+                        return false;
                     }
-                default: break;
-            }                
+                }
+            }
+            return true;
         }
     }
 }
